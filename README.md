6fcabde0fbb088fc870b64c14d9df589449071f0
Crowdedlight <crowdedhd@gmail.com>
2024-01-17 01:35:57 +0100

    fixed errors and bugs to pass ConformU

--- README.md
+++ README.md
@@ -1,5 +1,80 @@
 # EspFlatPanel
-Code for ESP32 and ASCOM driver for wireless FlatPanel
+Code for ESP32-S3 and ASCOM Alpaca driver for wireless Flat Panel running DotStar LED strip
 
 Libs required:
 ArduinoJson 7.0
+FastLED 3.6.0
+
+They should be installed automatically through platformio when loading this workspace
+
+
+Tested and verified with ASCOM ConformU to conforming to the Ascom Alpaca Standard.  
+The issues reported is due to ConformU skips testing Cover methods when Cover is "Not Implemented". I have raised the issue on the group forum. 
+
+```
+00:51:47.744 ASCOM Universal Device Conformance Checker Version 2.1.0.23787, Build time: tor 11 januar 2024 11:25:50
+00:51:47.745
+00:51:47.745 Operating system is Microsoft Windows 10.0.19045 64bit, Application is 64bit.
+00:51:47.746
+00:51:47.747 Alpaca device: Esp Flat Panel (192.168.11.19:4567 CoverCalibrator/0)
+00:51:47.748
+00:51:47.748 CreateDevice                        INFO     Creating Alpaca device: IP address: 192.168.11.19, IP Port: 4567, Alpaca device number: 0
+00:51:47.749 CreateDevice                        INFO     Alpaca device created OK
+00:51:47.750 CreateDevice                        INFO     Successfully created driver
+00:51:48.764 CreateDevice                        OK       Driver instance created successfully
+00:51:48.765
+00:51:48.765 Pre-connect checks
+00:51:48.766
+00:51:48.767 Connect to device
+00:51:48.944 Connected                           OK       Connected to device successfully using Connected = True
+00:51:48.945
+00:51:48.946 Common Driver Methods
+00:51:48.947 InterfaceVersion                    OK       1
+00:51:48.984 Connected                           OK       True
+00:51:49.018 Description                         OK       ESP32 Alpaca controlled flat-panel for use with DotStar LED strip and a homemade flatpanel
+00:51:49.060 DriverInfo                          OK       Driver is firmware made by Crowdedlight for ESP32-S3: https://github.com/Crowdedlight/EspFlatPanel
+00:51:49.095 DriverVersion                       OK       v1.0.0
+00:51:49.123 Name                                OK       Esp Flat Panel
+00:51:49.124
+00:51:49.124 Action                              INFO     Conform cannot test the Action method
+00:51:49.170 SupportedActions                    OK       Driver returned an empty action list
+00:51:49.171
+00:51:49.171 DeviceState                         INFO     DeviceState tests omitted - DeviceState is not available in this interface version.
+00:51:49.172
+00:51:49.172 Properties
+00:51:49.207 CalibratorState                     OK       Off
+00:51:49.254 CoverState                          OK       Received a NotImplementedException from an Alpaca device as expected
+00:51:49.331 MaxBrightness                       OK       255
+00:51:49.404 Brightness                          OK       255
+00:51:49.405
+00:51:49.405 Methods
+00:51:49.406 OpenCover                           ISSUE    Test skipped because CoverState returned an exception
+00:51:49.407 CloseCover                          ISSUE    Test skipped because CoverState returned an exception
+00:51:49.408 HaltCover                           ISSUE    Test skipped because CoverState returned an exception
+00:51:49.458 CalibratorOn                        OK       CalibratorOn with brightness -1 threw an InvalidValueException as expected
+00:51:49.562 CalibratorOn                        OK       CalibratorOn with brightness 0 was successful. The synchronous operation took 0,1 seconds
+00:51:49.604 CalibratorOn                        OK       The Brightness property does return the value that was set
+00:51:49.740 CalibratorOn                        OK       CalibratorOn with brightness 63 was successful. The synchronous operation took 0,1 seconds
+00:51:49.759 CalibratorOn                        OK       The Brightness property does return the value that was set
+00:51:49.885 CalibratorOn                        OK       CalibratorOn with brightness 127 was successful. The synchronous operation took 0,1 seconds
+00:51:49.928 CalibratorOn                        OK       The Brightness property does return the value that was set
+00:51:50.029 CalibratorOn                        OK       CalibratorOn with brightness 191 was successful. The synchronous operation took 0,1 seconds
+00:51:50.068 CalibratorOn                        OK       The Brightness property does return the value that was set
+00:51:50.166 CalibratorOn                        OK       CalibratorOn with brightness 255 was successful. The synchronous operation took 0,1 seconds
+00:51:50.204 CalibratorOn                        OK       The Brightness property does return the value that was set
+00:51:50.235 CalibratorOn                        OK       CalibratorOn with brightness 256 threw an InvalidValueException as expected
+00:51:50.392 CalibratorOff                       OK       CalibratorOff was successful. The synchronous action took 0,2 seconds
+00:51:50.429 CalibratorOff                       OK       Brightness is set to zero when the calibrator is turned off
+00:51:50.430
+00:51:50.431 Disconnect from device
+00:51:50.523 Connected                           OK       False
+00:51:50.524
+00:51:50.524 Conformance test has finished
+00:51:50.525
+00:51:50.526 Your device had 3 issues, 0 errors and 0 configuration alerts
+00:51:50.527
+00:51:50.527 Issue Summary
+00:51:50.528 OpenCover                           ISSUE    Test skipped because CoverState returned an exception
+00:51:50.529 CloseCover                          ISSUE    Test skipped because CoverState returned an exception
+00:51:50.530 HaltCover                           ISSUE    Test skipped because CoverState returned an exception
+```
--- src/zmain.cpp
+++ src/zmain.cpp
@@ -10,7 +10,7 @@
 #include "FastLED.h"
 
 // enable debugging values
-const bool DEBUG = true;
+const bool DEBUG = false;
 
 const char *ssid = _SSID;           // your network SSID (name)
 const char *password = _PASSWORD;   // your network password (use for WPA, or use as key for WEP)
@@ -18,6 +18,16 @@ const char *password = _PASSWORD;   // your network password (use for WPA, or us
 unsigned const int discoveryPort = 32227;  //The Alpaca Discovery port
 unsigned const int alpacaPort = 4567;  //The (fake) port that the Alpaca API would be available on
 const char *uuid = "8de70ae6-ce7b-4741-ae69-1294a316e906";
+
+// Device Specific information
+String deviceNameNoSpace = "EspFlatPanel";
+String Manufacturer = "Crowdedlight";
+String Version = "v1.0.0";
+String ManufacturerLocation = "The Milky Way";
+String DeviceName = "Esp Flat Panel";
+String DeviceType = "CoverCalibrator";
+String DeviceDescription = "ESP32 Alpaca controlled flat-panel for use with DotStar LED strip and a homemade flatpanel";
+String DriverInfo = "Driver is firmware made by Crowdedlight for ESP32-S3: https://github.com/Crowdedlight/EspFlatPanel";
 
 AsyncUDP udp_discovery;
 WebServer server(alpacaPort);
@@ -54,6 +64,7 @@ CRGB NEUTRAL_WHITE = CRGB( 252, 250, 246);
 // default is that the LEDs are off, waiting for commands to turn on
 unsigned int MaxBrightness = 255;
 unsigned int CalibratorState = States::OFF;
+bool alpacaConnected = false;
 
 /*
 ******* REMEMBER TO EDIT CONFIG.H FOR YOUR CONFIGURATION *******
@@ -61,10 +72,23 @@ unsigned int CalibratorState = States::OFF;
 
 // API callbacks
 void handleNotFound();
+void handleNotImplemented();
+void handleTransactionAndClientIds(JsonDocument &doc);
+// Common 
+void connectedPutRequest();
+void connectedGetRequest();
+void SimpleDescriptionRequest();
+// Management and Protocol specific
 void discoveryRequest(AsyncUDPPacket &packet);
 void ConfiguredDevicesRequest();
 void ApiVersionsRequest();
 void DescriptionRequest();
+void DriverInfoRequest();
+void DriverVersionRequest();
+void InterfaceVersionRequest();
+void nameRequest();
+void supportedActionsRequest();
+// Type specific
 void BrightnessRequest();
 void CalibratorStateRequest();
 void CoverStateRequest();
@@ -94,6 +118,7 @@ void setup ()
   FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);  // BGR ordering is typical
   // clear all data and push to LEDs. So we start with them turned off. 
   FastLED.clear(true);
+  FastLED.setBrightness(0);
 
   // setup WIFI
   WiFi.mode(WIFI_STA);
@@ -122,17 +147,31 @@ void setup ()
   server.on("/management/v1/configureddevices", HTTP_GET, ConfiguredDevicesRequest);
   server.on("/management/apiversions", HTTP_GET, ApiVersionsRequest);
   server.on("/management/v1/description", HTTP_GET, DescriptionRequest);
+  // common alpaca methods shared across all device types
+  server.on("/api/v1/covercalibrator/0/connected", HTTP_PUT, connectedPutRequest);
+  server.on("/api/v1/covercalibrator/0/connected", HTTP_GET, connectedGetRequest);
+  server.on("/api/v1/covercalibrator/0/description", HTTP_GET, SimpleDescriptionRequest);
+  server.on("/api/v1/covercalibrator/0/driverinfo", HTTP_GET, DriverInfoRequest);
+  server.on("/api/v1/covercalibrator/0/driverversion", HTTP_GET, DriverVersionRequest);
+  server.on("/api/v1/covercalibrator/0/interfaceversion", HTTP_GET, InterfaceVersionRequest);
+  server.on("/api/v1/covercalibrator/0/name", HTTP_GET, nameRequest);
+  server.on("/api/v1/covercalibrator/0/supportedactions", HTTP_GET, supportedActionsRequest);
+
+  // not implemented apis for cover
+  server.on("/api/v1/covercalibrator/0/opencover", HTTP_PUT, handleNotImplemented);
+  server.on("/api/v1/covercalibrator/0/closecover", HTTP_PUT, handleNotImplemented);
+  server.on("/api/v1/covercalibrator/0/haltcover", HTTP_PUT, handleNotImplemented);
 
   // Command API For CoverCalibrator
   // We can hardcode the device-number, as we only support 1 device for now. 
   // Only implementing the brightness, states, and max brightness api calls. Everything else will go to "nothandled" and return appropiate error of not implemented.
-  server.on("/covercalibrator/0/brightness", HTTP_GET, BrightnessRequest);
-  server.on("/covercalibrator/0/calibratorstate", HTTP_GET, CalibratorStateRequest);
-  server.on("/covercalibrator/0/coverstate", HTTP_GET, CoverStateRequest); // just returns 200 "NotPresent	int: 0	This device does not have a cover that can be closed independently "
-  server.on("/covercalibrator/0/maxbrightness", HTTP_GET, MaxBrightnessRequest);
+  server.on("/api/v1/covercalibrator/0/brightness", HTTP_GET, BrightnessRequest);
+  server.on("/api/v1/covercalibrator/0/calibratorstate", HTTP_GET, CalibratorStateRequest);
+  server.on("/api/v1/covercalibrator/0/coverstate", HTTP_GET, CoverStateRequest); // just returns 200 "NotPresent	int: 0	This device does not have a cover that can be closed independently "
+  server.on("/api/v1/covercalibrator/0/maxbrightness", HTTP_GET, MaxBrightnessRequest);
   // put requests aka. Actual control of the calibrator
-  server.on("/covercalibrator/0/calibratoroff", HTTP_PUT, CalibratorOffRequest);
-  server.on("/covercalibrator/0/calibratoron", HTTP_PUT, CalibratorOnRequest);
+  server.on("/api/v1/covercalibrator/0/calibratoroff", HTTP_PUT, CalibratorOffRequest);
+  server.on("/api/v1/covercalibrator/0/calibratoron", HTTP_PUT, CalibratorOnRequest);
 
   // TODO implement the last API calls, including the LED state specific stuff. Need to have an LED state to return
 
@@ -161,22 +200,24 @@ void loop ()
 
 
 void discoveryRequest(AsyncUDPPacket &packet) {
-  // TODO remove unnessecary debug prints
-  Serial.print("Received UDP Packet of Type: ");
-  Serial.print(packet.isBroadcast() ? "Broadcast" : packet.isMulticast() ? "Multicast" : "Unicast");
-  Serial.print(", From: ");
-  Serial.print(packet.remoteIP());
-  Serial.print(":");
-  Serial.print(packet.remotePort());
-  Serial.print(", To: ");
-  Serial.print(packet.localIP());
-  Serial.print(":");
-  Serial.print(packet.localPort());
-  Serial.print(", Length: ");
-  Serial.print(packet.length());
-  Serial.print(", Data: ");
-  Serial.write(packet.data(), packet.length());
-  Serial.println();
+  // debug message
+  if (DEBUG) {
+    Serial.print("Received UDP Packet of Type: ");
+    Serial.print(packet.isBroadcast() ? "Broadcast" : packet.isMulticast() ? "Multicast" : "Unicast");
+    Serial.print(", From: ");
+    Serial.print(packet.remoteIP());
+    Serial.print(":");
+    Serial.print(packet.remotePort());
+    Serial.print(", To: ");
+    Serial.print(packet.localIP());
+    Serial.print(":");
+    Serial.print(packet.localPort());
+    Serial.print(", Length: ");
+    Serial.print(packet.length());
+    Serial.print(", Data: ");
+    Serial.write(packet.data(), packet.length());
+    Serial.println();
+  }
 
   // No undersized packets allowed
   if (packet.length() < 16)
@@ -207,6 +248,29 @@ void handleNotFound() {
   Serial.println(message); // debug to see what they ask of us that isn't implemented!
 }
 
+void handleTransactionAndClientIds(JsonDocument &doc) {
+  if (server.hasArg("ClientID")) {
+    doc["ClientID"] = server.arg("ClientID").toInt();
+  }  
+  if (server.hasArg("ClientTransactionID")) {
+    doc["ClientTransactionID"] = server.arg("ClientTransactionID").toInt();
+  }
+}
+
+void handleNotImplemented() {
+  JsonDocument doc;
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+  JsonObject root = doc.to<JsonObject>();
+  doc["ErrorNumber"] = 0x400; // NOT IMPLEMENTED ERROR CODE
+  doc["ErrorMessage"] = "This device has no cover connected";
+
+  String output;
+  serializeJson(doc, output);
+
+  server.send(200, "application/json", output);
+}
+
 void ApiVersionsRequest() {
   Serial.println("Received 'ApiVersions' request");
   Serial.flush();
@@ -214,12 +278,7 @@ void ApiVersionsRequest() {
   JsonDocument doc;
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
+  handleTransactionAndClientIds(doc);
 
   JsonArray value = doc["value"].to<JsonArray>();
   value.add(1);
@@ -231,6 +290,202 @@ void ApiVersionsRequest() {
   server.send(200, "application/json", output);
 }
 
+void connectedGetRequest() {
+  Serial.println("Received 'Connected' Get request");
+  Serial.flush();
+
+  JsonDocument doc;
+
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+
+  JsonObject root = doc.to<JsonObject>();
+  doc["Value"] = alpacaConnected;
+
+  String output;
+  serializeJson(doc, output);
+
+  if (DEBUG) {
+    serializeJson(doc, Serial);
+  }
+
+  server.send(200, "application/json", output);
+}
+
+void connectedPutRequest() {
+  Serial.println("Received 'Connected' Put request");
+  Serial.flush();
+
+  JsonDocument doc;
+
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+
+  // check argument, if brightness is within range and exist, we turn on, otherwise we throw error
+  if (!server.hasArg("Connected")) {
+    server.send(400, "text/plain", "Missing Connected value");
+    return;
+  }
+
+  // get bool from request
+  bool newStatus = false;
+  String reqStatus = server.arg("Connected");
+
+  if (reqStatus.equalsIgnoreCase("true")) {
+    newStatus = true;
+  }
+
+  // set connected state
+  alpacaConnected = newStatus;
+
+  doc["ErrorNumber"] = 0;
+  doc["ErrorMessage"] = "";
+
+  String output;
+  serializeJson(doc, output);
+
+  if (DEBUG) {
+    serializeJson(doc, Serial);
+  }
+
+  server.sendHeader("Cache-Control", "no-cache");
+  server.send(200, "application/json", output);
+}
+
+void SimpleDescriptionRequest() {
+  Serial.println("Received 'simple description' request");
+  Serial.flush();
+
+  JsonDocument doc;
+
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+
+  JsonObject root = doc.to<JsonObject>();
+  doc["Value"] = DeviceDescription;
+
+  String output;
+  serializeJson(doc, output);
+
+  if (DEBUG) {
+    serializeJson(doc, Serial);
+  }
+
+  server.send(200, "application/json", output);
+}
+
+void DriverInfoRequest() {
+  Serial.println("Received 'driverinfo' request");
+  Serial.flush();
+
+  JsonDocument doc;
+
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+
+  JsonObject root = doc.to<JsonObject>();
+  doc["Value"] = DriverInfo;
+
+  String output;
+  serializeJson(doc, output);
+
+  if (DEBUG) {
+    serializeJson(doc, Serial);
+  }
+
+  server.send(200, "application/json", output);
+}
+
+void DriverVersionRequest() {
+  Serial.println("Received 'driverversion' request");
+  Serial.flush();
+
+  JsonDocument doc;
+
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+
+  JsonObject root = doc.to<JsonObject>();
+  doc["Value"] = Version;
+
+  String output;
+  serializeJson(doc, output);
+
+  if (DEBUG) {
+    serializeJson(doc, Serial);
+  }
+
+  server.send(200, "application/json", output);
+}
+
+void InterfaceVersionRequest() {
+  Serial.println("Received 'interfaceversion' request");
+  Serial.flush();
+
+  JsonDocument doc;
+
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+
+  JsonObject root = doc.to<JsonObject>();
+  doc["Value"] = 1;
+
+  String output;
+  serializeJson(doc, output);
+
+  if (DEBUG) {
+    serializeJson(doc, Serial);
+  }
+
+  server.send(200, "application/json", output);
+}
+
+void nameRequest() {
+  Serial.println("Received 'name' request");
+  Serial.flush();
+
+  JsonDocument doc;
+
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+
+  JsonObject root = doc.to<JsonObject>();
+  doc["Value"] = DeviceName;
+
+  String output;
+  serializeJson(doc, output);
+
+  if (DEBUG) {
+    serializeJson(doc, Serial);
+  }
+
+  server.send(200, "application/json", output);
+}
+
+void supportedActionsRequest() {
+  Serial.println("Received 'supportedActions' request");
+  Serial.flush();
+
+  JsonDocument doc;
+
+  // handle arguments/parameters of: ClientID & ClientTransactionID
+  handleTransactionAndClientIds(doc);
+
+  JsonObject root = doc.to<JsonObject>();
+  JsonArray value = doc["Value"].to<JsonArray>();
+  // This is to add custom actions that is otherwise not described in the alpaca protocol as I understand it. So could potentially add "SetColour" or similar to it. Sending no custom entries for now. 
+  //value.add("");
+
+  String output;
+  serializeJson(doc, output);
+
+  if (DEBUG) {
+    serializeJson(doc, Serial);
+  }
+
+  server.send(200, "application/json", output);
+}
+
 void DescriptionRequest() {
   Serial.println("Received 'Description' request");
   Serial.flush();
@@ -238,19 +493,14 @@ void DescriptionRequest() {
   JsonDocument doc;
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
+  handleTransactionAndClientIds(doc);
 
   JsonObject root = doc.to<JsonObject>();
   JsonObject value = doc["Value"].to<JsonObject>();
-  value["ServerName"] = "EspFlatPanel";
-  value["Manufacturer"] = "Crowdedlight";
-  value["ManufacturerVersion"] = "v1.0.0";
-  value["Location"] = "The Milky Way";
+  value["ServerName"] = deviceNameNoSpace;
+  value["Manufacturer"] = Manufacturer;
+  value["ManufacturerVersion"] = Version;
+  value["Location"] = ManufacturerLocation;
 
   String output;
   serializeJson(doc, output);
@@ -269,18 +519,13 @@ void ConfiguredDevicesRequest() {
   JsonDocument doc;
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
+  handleTransactionAndClientIds(doc);
 
   JsonObject root = doc.to<JsonObject>();
   JsonArray value = doc["Value"].to<JsonArray>();
   JsonObject object = value.add<JsonObject>();
-  object["DeviceName"] = "Esp Flat Panel";
-  object["DeviceType"] = "CoverCalibrator";
+  object["DeviceName"] = DeviceName;
+  object["DeviceType"] = DeviceType;
   object["DeviceNumber"] = 0;
   object["UniqueID"] = uuid;
 
@@ -300,16 +545,11 @@ void BrightnessRequest() {
   Serial.flush();
 
   JsonDocument doc;
-  JsonObject root = doc.to<JsonObject>();
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
+  handleTransactionAndClientIds(doc);
 
+  JsonObject root = doc.to<JsonObject>();
   doc["ErrorNumber"] = 0;
   doc["ErrorMessage"] = "";
   doc["Value"] = FastLED.getBrightness();  
@@ -330,16 +570,11 @@ void CalibratorStateRequest() {
   Serial.flush();
 
   JsonDocument doc;
-  JsonObject root = doc.to<JsonObject>();
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
-
+  handleTransactionAndClientIds(doc);
+  
+  JsonObject root = doc.to<JsonObject>();
   doc["ErrorNumber"] = 0;
   doc["ErrorMessage"] = "";
   doc["Value"] = CalibratorState;  
@@ -360,19 +595,14 @@ void CoverStateRequest() {
   Serial.flush();
 
   JsonDocument doc;
-  JsonObject root = doc.to<JsonObject>();
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
+  handleTransactionAndClientIds(doc);
 
-  doc["ErrorNumber"] = 0;
-  doc["ErrorMessage"] = "";
-  doc["Value"] = States::NOT_PRESENT;  
+  JsonObject root = doc.to<JsonObject>();
+  doc["ErrorNumber"] = 0x400;
+  doc["ErrorMessage"] = "This device has no cover";
+  // doc["Value"] = States::NOT_PRESENT;  
 
   String output;
   serializeJson(doc, output);
@@ -390,16 +620,11 @@ void MaxBrightnessRequest() {
   Serial.flush();
 
   JsonDocument doc;
-  JsonObject root = doc.to<JsonObject>();
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
+  handleTransactionAndClientIds(doc);
 
+  JsonObject root = doc.to<JsonObject>();
   doc["ErrorNumber"] = 0;
   doc["ErrorMessage"] = "";
   doc["Value"] = MaxBrightness;  
@@ -424,18 +649,13 @@ void CalibratorOffRequest() {
   TurnOff();
 
   JsonDocument doc;
-  JsonObject root = doc.to<JsonObject>();
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
+  handleTransactionAndClientIds(doc);
 
+  JsonObject root = doc.to<JsonObject>();
   doc["ErrorNumber"] = 0;
-  doc["ErrorMessage"] = "Failed updating the LEDs";
+  doc["ErrorMessage"] = "";
 
   String output;
   serializeJson(doc, output);
@@ -453,15 +673,11 @@ void CalibratorOnRequest() {
   Serial.flush();
 
   JsonDocument doc;
-  JsonObject root = doc.to<JsonObject>();
 
   // handle arguments/parameters of: ClientID & ClientTransactionID
-  if (server.hasArg("ClientID")) {
-    doc["ClientID"] = server.arg("ClientID");
-  }  
-  if (server.hasArg("ClientTransactionID")) {
-    doc["ClientTransactionID"] = server.arg("ClientTransactionID");
-  }
+  handleTransactionAndClientIds(doc);
+
+  JsonObject root = doc.to<JsonObject>();
 
   int err_code = 0;
   String error_msg = "";
@@ -475,8 +691,8 @@ void CalibratorOnRequest() {
   int brightness = server.arg("Brightness").toInt();
 
   // if operation was not true, then we set error here
-  if (brightness < 0 && brightness > 255) {
-    err_code = 0x80040401;
+  if (brightness < 0 || brightness > 255) {
+    err_code = 0x401; // invalid value
     error_msg = "brightness outside range 0-255";
   } else {
     // no errors, so we comply with command
@@ -502,8 +718,11 @@ void TurnOn(int brightness) {
   fill_solid(leds, NUM_LEDS, NEUTRAL_WHITE);
   FastLED.setBrightness(brightness);
   FastLED.show();
+  CalibratorState = States::READY;
 }
 
 void TurnOff() {
   FastLED.clear(true);
+  FastLED.setBrightness(0);
+  CalibratorState = States::OFF;
 }
